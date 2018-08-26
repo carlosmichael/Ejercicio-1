b34470359251a93c10b6b4adc1cf16e518aac9ac
Mategas <Mategas@users.noreply.github.com>
2018-08-26 19:22:20 -0300

    Subido ejercicio 1
    Ejercicio 1

---
+++ Ej1/main.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+    int letra;
+
+    printf("\n\n\n");
+    printf("Ingresar letra:\n");
+    letra = getchar();
+
+    printf("La letra ingresada fue: ");
+    putchar(letra);
+    return 0;
+}s

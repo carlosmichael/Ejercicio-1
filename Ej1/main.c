56d05183c754ad0aa416b8b0136b16fecb1c6fd9
Huamán <carlosmichael@users.noreply.github.com>
2018-08-26 19:35:11 -0300

    Agregada suma

--- Ej1/main.c
+++ Ej1/main.c
@@ -6,6 +6,13 @@ int main()
 {
     int letra;
 
+    int n1 = 30;
+    int n2 = 10;
+
+    int suma= n1+n2;
+
+    printf("%d",suma);
+
     printf("\n\n\n");
     printf("Ingresar letra:\n");
     letra = getchar();
@@ -13,4 +20,4 @@ int main()
     printf("La letra ingresada fue: ");
     putchar(letra);
     return 0;
-}s
+}
